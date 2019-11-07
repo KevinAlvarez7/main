a26c893a6d0c1bda7dc5193e46f3a6ebcacb7c24
Kevin Alvarez <kevinalvarez373@gmail.com>
2019-11-07 23:14:15 +0800

    test v1.5

--- src/main/java/duke/parser/FindCommandParser.java
+++ src/main/java/duke/parser/FindCommandParser.java
@@ -1,4 +1,10 @@
 package duke.parser;
 
+import duke.logic.commands.Command;
+import duke.logic.commands.ListCommand;
+
 public class FindCommandParser {
+    public Command parse() {
+        return new ListCommand();
+    }
 }
